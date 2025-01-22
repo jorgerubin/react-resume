61552789edca6f82cf2ba633382d2af44cca35ee
jorge <jorgerubin271@gmail.com>
2025-01-22 20:25:56 +0100

    update: test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import { render, screen, fireEvent  } from '@testing-library/react';
 import App from './App';
+import data from "./data.json";
 
-test('Cambiar tema a claro', () => {
-
-  // Mock matchMedia para simular "dark mode"
+// Mock matchMedia para simular "dark mode"
   window.matchMedia = jest.fn().mockImplementation((query) => ({
     matches: query === "(prefers-color-scheme: dark)",
     media: query,
@@ -12,25 +11,46 @@ test('Cambiar tema a claro', () => {
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
   }));
-  
+
+/*
+test('Cambiar tema a claro', () => {
   const { contenedor } = render(<App />);
   const botonTemas = contenedor.querySelector('.theme-switcher');
   fireEvent.click(botonTemas);
   const linkElement = screen.getByText(/Dark Mode/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-/*
-test('Estan los link bien', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-test('Los datos estan Bien', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 */
+
+test('aplicar tema por defecto', () => {
+  // Mock `window.matchMedia`
+  window.matchMedia = jest.fn().mockImplementation((query) => {
+    return {
+      matches: query === '(prefers-color-scheme: dark)',
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  });
+
+  render(<App />);
+
+  const htmlElement = document.documentElement;
+  const appliedTheme = htmlElement.getAttribute('data-theme');
+
+  expect(['light', 'dark']).toContain(appliedTheme);
+});
+
+describe('Todos los datos estan en regla', () => {
+
+  it('renderiza toda la informacion', () => {
+    render(<App />);
+
+    // Verificar que los componentes clave están presentes
+    expect(screen.getByText(data.info.name)).toBeInTheDocument(); // Header
+    expect(screen.getByText(data.experience[0].company)).toBeInTheDocument(); // ExperienceSection
+    expect(screen.getByText(data.skills[0].name)).toBeInTheDocument(); // SkillsSection
+    expect(screen.getByText(data.education[0].degree)).toBeInTheDocument(); // EducationSection
+  });
+});
