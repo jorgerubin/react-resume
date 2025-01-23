e55701c92b797fd11d8b0d0f6111b54fc652b0e8
jorge <jorgerubin271@gmail.com>
2025-01-23 15:43:19 +0100

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent  } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 import data from "./data.json";
 
