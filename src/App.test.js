fa393f7aa08036cf38bd6a5c5f9abc3117bcf983
jorge <jorgerubin271@gmail.com>
2025-01-23 15:42:45 +0100

    update:tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,26 +2,6 @@ import { render, screen, fireEvent  } from '@testing-library/react';
 import App from './App';
 import data from "./data.json";
 
-// Mock matchMedia para simular "dark mode"
-  window.matchMedia = jest.fn().mockImplementation((query) => ({
-    matches: query === "(prefers-color-scheme: dark)",
-    media: query,
-    onchange: null,
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  }));
-
-/*
-test('Cambiar tema a claro', () => {
-  const { contenedor } = render(<App />);
-  const botonTemas = contenedor.querySelector('.theme-switcher');
-  fireEvent.click(botonTemas);
-  const linkElement = screen.getByText(/Dark Mode/i);
-  expect(linkElement).toBeInTheDocument();
-});
-*/
-
 test('aplicar tema por defecto', () => {
   // Mock `window.matchMedia`
   window.matchMedia = jest.fn().mockImplementation((query) => {
@@ -42,15 +22,12 @@ test('aplicar tema por defecto', () => {
   expect(['light', 'dark']).toContain(appliedTheme);
 });
 
-describe('Todos los datos estan en regla', () => {
+test('renderiza toda la informacion', () => {
+  render(<App />);
 
-  it('renderiza toda la informacion', () => {
-    render(<App />);
-
-    // Verificar que los componentes clave están presentes
-    expect(screen.getByText(data.info.name)).toBeInTheDocument(); // Header
-    expect(screen.getByText(data.experience[0].company)).toBeInTheDocument(); // ExperienceSection
-    expect(screen.getByText(data.skills[0].name)).toBeInTheDocument(); // SkillsSection
-    expect(screen.getByText(data.education[0].degree)).toBeInTheDocument(); // EducationSection
-  });
+  // Verificar que los componentes clave están presentes
+  expect(screen.getByText(data.info.name)).toBeInTheDocument(); // Header
+  expect(screen.getByText(data.experience[0].company)).toBeInTheDocument(); // ExperienceSection
+  expect(screen.getByText(data.skills[0].name)).toBeInTheDocument(); // SkillsSection
+  expect(screen.getByText(data.education[0].degree)).toBeInTheDocument(); // EducationSection
 });
